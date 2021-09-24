50eab7e803287da4b5183b941fc80940fdc53e07
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-24 09:02:26 +0200

    add html and basic scss style

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,34 @@ import '../styles/App.scss';
 
 function App() {
   return (
-    <div>
-      <h1>Hola mundo</h1>
+    <div className='page'>
+      <header className='header'>
+        <h1 className='header--title'>Mis clubs</h1>
+      </header>
+      <section>
+        <ul className='list__container'>
+          <li className='list__container--li'>
+            <h3>#0 Book club</h3>
+            <p>Abierto entre semana: Sí</p>
+            <p>Abierto el fin de semana: Sí</p>
+          </li>
+          <li className='list__container--li'>
+            <h3>#1 Chess club</h3>
+            <p>Abierto entre semana: No</p>
+            <p>Abierto el fin de semana: Sí</p>
+          </li>
+          <li className='list__container--li'>
+            <h3>#2 Escape room club</h3>
+            <p>Abierto entre semana: No</p>
+            <p>Abierto el fin de semana: No</p>
+          </li>
+          <li className='list__container--li'>
+            <h3>#3 Debate club</h3>
+            <p>Abierto entre semana: Sí</p>
+            <p>Abierto el fin de semana: No</p>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
