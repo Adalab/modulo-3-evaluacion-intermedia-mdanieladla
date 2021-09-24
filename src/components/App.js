a111ba7a18b889958a0a219602982483f609fdf6
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-24 10:17:22 +0200

    add new club

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,55 @@ import { useState } from 'react';
 
 function App() {
   const [clubs, setClubs] = useState(clubsData);
-  const openWeeksDays = 'Abierto entre semana: Sí';
-  const notOpenWeeksDays = 'Abierto entre semana: No';
-  const openWeekends = 'Abierto el fin de semana: Sí';
-  const notOpenWeekends = 'Abierto el fin de semana: No';
+  const [newClubName, setNewClub] = useState('');
+  const [weekDayOpen, setWeekDayOpen] = useState('');
+  const [weekendOpen, setWeekendOpen] = useState('');
+  // const openWeekDays = 'Abierto entre semana';
+  // const notOpenWeekDays = 'Abierto entre semana: No';
+  // const openWeekends = 'Abierto el fin de semana: Sí';
+  // const notOpenWeekends = 'Abierto el fin de semana: No';
 
+  //Pintar en el HTML las tarjetas
   const htmlClubsList = clubs.map((oneClub, index) => (
     <li key={index} className='list__container--li'>
       <h3>
         #{index} {oneClub.name}
       </h3>
-      <p>{openWeeksDays} </p>
-      <p>{openWeekends}</p>
+      <p>
+        Abierto entre semana : {oneClub.openOnWeekDays === true ? 'Si' : 'No'}
+      </p>
+      <p>
+        Abierto los fines de semana :
+        {oneClub.openOnWeekend === true ? 'Si' : 'No'}
+      </p>
     </li>
   ));
+
+  //Coger el valor de los inputs
+  const handleChangeClubName = (ev) => {
+    setNewClub(ev.currentTarget.value);
+  };
+
+  const handleChangeOpenDays = (ev) => {
+    setWeekDayOpen(ev.currentTarget.checked);
+  };
+
+  const handleChangeWeekend = (ev) => {
+    setWeekendOpen(ev.currentTarget.checked);
+  };
+
+  //Añadir nuevo club al hacer click y que no se refresque la página.
+  const handleClick = (ev) => {
+    ev.preventDefault();
+
+    const newClub = {
+      name: newClubName,
+      openOnWeekDays: weekDayOpen,
+      openOnWeekend: weekendOpen,
+    };
+
+    setClubs([...clubs, newClub]);
+  };
 
   return (
     <div className='page'>
@@ -30,15 +65,28 @@ function App() {
       <h4 className='title--newClub'>Añadir un nuevo club</h4>
       <form className='form'>
         <label> Nombre del club </label>
-        <input type='text' className='form__input--name' />
+        <input
+          type='text'
+          className='form__input--name'
+          onChange={handleChangeClubName}
+        />
         <label>¿Abre entre semana?</label>
-        <input type='checkbox' className='form__input--check' />
+        <input
+          type='checkbox'
+          className='form__input--check'
+          onClick={handleChangeOpenDays}
+        />
         <label>¿Abre los fines de semana?</label>
-        <input type='checkbox' className='form__input--check' />
+        <input
+          type='checkbox'
+          className='form__input--check'
+          onClick={handleChangeWeekend}
+        />
         <input
           type='submit'
           value='Añadir un nuevo club'
           className='form__input--btn'
+          onClick={handleClick}
         />
       </form>
     </div>
