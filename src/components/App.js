0002dc5725e73b5f187d9495238f46cff5035869
mdanieladla <mariadanieladarnea@gmail.com>
2021-09-24 09:48:07 +0200

    modified scss style, render clubs and add form

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,46 @@
 import '../styles/App.scss';
+import clubsData from '../data/clubsData.json';
+import { useState } from 'react';
 
 function App() {
+  const [clubs, setClubs] = useState(clubsData);
+  const openWeeksDays = 'Abierto entre semana: Sí';
+  const notOpenWeeksDays = 'Abierto entre semana: No';
+  const openWeekends = 'Abierto el fin de semana: Sí';
+  const notOpenWeekends = 'Abierto el fin de semana: No';
+
+  const htmlClubsList = clubs.map((oneClub, index) => (
+    <li key={index} className='list__container--li'>
+      <h3>
+        #{index} {oneClub.name}
+      </h3>
+      <p>{openWeeksDays} </p>
+      <p>{openWeekends}</p>
+    </li>
+  ));
+
   return (
     <div className='page'>
       <header className='header'>
         <h1 className='header--title'>Mis clubs</h1>
       </header>
       <section>
-        <ul className='list__container'>
-          <li className='list__container--li'>
-            <h3>#0 Book club</h3>
-            <p>Abierto entre semana: Sí</p>
-            <p>Abierto el fin de semana: Sí</p>
-          </li>
-          <li className='list__container--li'>
-            <h3>#1 Chess club</h3>
-            <p>Abierto entre semana: No</p>
-            <p>Abierto el fin de semana: Sí</p>
-          </li>
-          <li className='list__container--li'>
-            <h3>#2 Escape room club</h3>
-            <p>Abierto entre semana: No</p>
-            <p>Abierto el fin de semana: No</p>
-          </li>
-          <li className='list__container--li'>
-            <h3>#3 Debate club</h3>
-            <p>Abierto entre semana: Sí</p>
-            <p>Abierto el fin de semana: No</p>
-          </li>
-        </ul>
+        <ul className='list__container'>{htmlClubsList}</ul>
       </section>
+      <h4 className='title--newClub'>Añadir un nuevo club</h4>
+      <form className='form'>
+        <label> Nombre del club </label>
+        <input type='text' className='form__input--name' />
+        <label>¿Abre entre semana?</label>
+        <input type='checkbox' className='form__input--check' />
+        <label>¿Abre los fines de semana?</label>
+        <input type='checkbox' className='form__input--check' />
+        <input
+          type='submit'
+          value='Añadir un nuevo club'
+          className='form__input--btn'
+        />
+      </form>
     </div>
   );
 }
